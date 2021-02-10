9c4bc9b15dedf708a29d3de5fd36d5c3f367d31a
gcf0082 <13510989573@sina.cn>
2021-02-10 12:38:16 +0000

    Deploying to gh-pages from  @ 23fac20a91376fe9f0f8ab1eed0727b53063e190 🚀

---
+++ precache-manifest.32f42d11e0f4c70bf429b4494f0e96b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4664962cae8d51e493c0c4172c315f",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "e4189c589aad791a92f6",
+    "url": "/github-actions-demo/static/css/main.866b4e6a.chunk.css"
+  },
+  {
+    "revision": "46fb897da663154444e6",
+    "url": "/github-actions-demo/static/js/2.032a9e91.chunk.js"
+  },
+  {
+    "revision": "e4189c589aad791a92f6",
+    "url": "/github-actions-demo/static/js/main.1db6dd23.chunk.js"
+  },
+  {
+    "revision": "41870333dfc813cf444c",
+    "url": "/github-actions-demo/static/js/runtime~main.dd1e6690.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
